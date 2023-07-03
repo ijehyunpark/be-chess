fc06cc9ef0a590d9da2fc16c81a5ec10f810f0d2
ijehyunpark <adam991217@naver.com>
2023-07-03 17:28:37 +0900

    Pawn 클래스 구현

---
+++ src/main/java/softeer2nd/Pawn.java
@@ -0,0 +1,12 @@
+package softeer2nd;
+
+public class Pawn {
+    private String color;
+    public Pawn(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+}
--- src/test/java/softeer2nd/PawnTest.java
+++ src/test/java/softeer2nd/PawnTest.java
@@ -8,10 +8,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 class PawnTest {
 
     @Test
-    @DisplayName("흰색 폰이 생성되어야 한다")
+    @DisplayName("흰색 및 검은색 폰이 생성되어야 한다")
     public void create() {
         Pawn pawn = new Pawn("white");
         assertThat(pawn.getColor()).isEqualTo("white");
+
+        Pawn pawn2 = new Pawn("black");
+        assertThat(pawn2.getColor()).isEqualTo("black");
     }
 
 
