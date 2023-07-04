36a2b6c8ff7198032079f8b4ec9709f2802e3cd9
ijehyunpark <adam991217@naver.com>
2023-07-04 11:00:59 +0900

    중복 제거

--- src/test/java/softeer2nd/chess/BoardTest.java
+++ src/test/java/softeer2nd/chess/BoardTest.java
@@ -1,5 +1,7 @@
 package softeer2nd.chess;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import softeer2nd.chess.pieces.Pawn;
@@ -7,19 +9,30 @@ import softeer2nd.chess.pieces.Pawn;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BoardTest {
+
+    Board board;
+    @BeforeEach
+    void setUp() {
+        board = new Board();
+    }
+
+    /**
+     * 보드판에 새로운 폰 객체를 추가하는 테스트 단위이다.
+     * @param color 추가할 폰 객체 색깔
+     */
+    void verifyAddPawnInBoard(final String color) {
+        int beforeSize = board.size();
+
+        Pawn pawn = new Pawn(color);
+        board.add(pawn);
+        assertEquals(beforeSize + 1, board.size());
+        assertEquals(pawn, board.findPawn(beforeSize));
+    }
+
     @Test
     @DisplayName("보드판 생성 테스트")
-    void create() throws Exception {
-        Board board = new Board();
-
-        Pawn white = new Pawn(Pawn.WHITE_COLOR);
-        board.add(white);
-        assertEquals(1, board.size());
-        assertEquals(white, board.findPawn(0));
-
-        Pawn black = new Pawn(Pawn.BLACK_COLOR);
-        board.add(black);
-        assertEquals(2, board.size());
-        assertEquals(black, board.findPawn(1));
+    void create(){
+        verifyAddPawnInBoard(Pawn.WHITE_COLOR);
+        verifyAddPawnInBoard(Pawn.BLACK_COLOR);
     }
 }
