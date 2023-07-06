a5e3b8e2a3ccd2a59e24b3addc51815ddfac2c2f
ijehyunpark <adam991217@naver.com>
2023-07-06 13:53:28 +0900

    Add King 이동 구현

--- src/main/java/softeer2nd/chess/Board/ChessGame.java
+++ src/main/java/softeer2nd/chess/Board/ChessGame.java
@@ -1,5 +1,6 @@
 package softeer2nd.chess.Board;
 
+import softeer2nd.chess.pieces.King;
 import softeer2nd.chess.pieces.Piece;
 
 import static softeer2nd.chess.pieces.Piece.Color.*;
@@ -41,7 +42,7 @@ public class ChessGame {
                 "rnbqkbnr";
 
     /**
-     * 기본 룰의 보드판을 초기화한다.
+     * 기본 룰의 보드판으로 초기화한다.
      */
     public static void initialize(Board board) {
         board.clear();
@@ -76,13 +77,37 @@ public class ChessGame {
 
 
     /**
-     * 보드판의 기물을 교환한다. <br/>
-     * 구체적으로 보드판에서 sourcePosition와 targetPosition의 위치를 교환한다.
+     * 보드판에서 한 기물을 이동시킨다. <br/>
+     * 구체적으로 가능한 이동일 경우 보드판에서 sourcePosition와 targetPosition의 위치를 교환한다.
      */
     public static void move(Board board, Board.Position sourcePosition, Board.Position targetPosition) {
+        if(targetPosition.getYPos() < 0 || targetPosition.getYPos() >= Board.COLUMN_NUMBER ||
+                targetPosition.getXPos() < 0 || targetPosition.getXPos() >= Board.ROW_NUMBER)
+            throw new IllegalArgumentException("보드판 범위를 벗어나는 입력입니다.");
+
         Piece source = board.findPiece(sourcePosition);
         Piece target = board.findPiece(targetPosition);
 
+        if(source.getColor() == target.getColor())
+            throw new IllegalArgumentException("같은 편 기물이 존재합니다.");
+
+        switch (source.getPieceType()){
+            case KING:
+                King.createBlackKing().move(board, sourcePosition, targetPosition);
+                break;
+            case QUEEN:
+                break;
+            case ROOK:
+                break;
+            case BISHOP:
+                break;
+            case KNIGHT:
+                break;
+            case PAWN:
+                break;
+            case NO_PIECE:
+                break;
+        }
         board.assignPiece(sourcePosition, target);
         board.assignPiece(targetPosition, source);
     }
