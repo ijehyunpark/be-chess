2ddee6fccbcd63917623a38fe3e6ed5bebcb279a
ijehyunpark <adam991217@naver.com>
2023-07-13 17:21:42 +0900

    Feat: 게임 종료 구현

--- src/test/java/softeer2nd/chess/GameManagerTest.java
+++ src/test/java/softeer2nd/chess/GameManagerTest.java
@@ -2,10 +2,10 @@ package softeer2nd.chess;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import softeer2nd.chess.Board.Board;
 import softeer2nd.chess.Board.Position;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static softeer2nd.chess.exception.ExceptionMessage.WRONG_TURN;
 import static softeer2nd.chess.utils.StringUtils.appendNewLine;
 
@@ -17,16 +17,16 @@ class GameManagerTest {
         //given
         GameManager chessGame = new GameManager();
         chessGame.start();
-        
+
         String expect = appendNewLine(
                 "RNBQKBNR",
-                        ".PPPPPPP",
-                        "P.......",
-                        "........",
-                        "........",
-                        "p.......",
-                        ".ppppppp",
-                        "rnbqkbnr"
+                ".PPPPPPP",
+                "P.......",
+                "........",
+                "........",
+                "p.......",
+                ".ppppppp",
+                "rnbqkbnr"
         );
 
         //when
@@ -47,8 +47,34 @@ class GameManagerTest {
         //then
         assertEquals(WRONG_TURN, wrongTurnExceptionBlack.getMessage());
         assertEquals(WRONG_TURN, wrongTurnExceptionWhite.getMessage());
-        
-        assertEquals(expect, chessGame.showBoard());
 
+        assertEquals(expect, chessGame.showBoard());
     }
+
+    @Test
+    @DisplayName("상대 편 킹 기물이 제거 여부를 반환하는지 테스트")
+    void checkKingCount() {
+        //given
+        String sample = appendNewLine(
+                "RNBQKBNR",
+                "PPPPPPPP",
+                ".....k..",
+                "........",
+                "........",
+                "........",
+                "........",
+                "........"
+        );
+        Board board = new Board();
+        board.initialize(sample);
+        GameManager chessGame = new GameManager(board);
+        chessGame.start();
+
+        //when
+        boolean isFinish = chessGame.move(new Position("e7"), new Position("f6"));
+
+        //then
+        assertTrue(isFinish);
+    }
+
 }
